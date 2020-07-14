0a511a0fe5e4346586888ace3ad7aee774883e85
Alicia Mileena Javier <aliciajavier5@gmail.com>
2020-07-14 01:24:02 +0000

    Done.

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -31,8 +31,7 @@ end
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
-  constants = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
-  constants[1][:name] =  "Joelle van Dyne (PGOAT)"
-  constants
+  aoh[row][key] = new_value
+  aoh
   
 end
